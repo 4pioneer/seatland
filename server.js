79804e4288824ffd7c4caed276ac68112fc94637
choi.sungryeol <choi.sungryeol@navercorp.com>
2021-12-25 13:17:09 +0900

    test: add leaflet example

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const app = express()
 const port = 3000
 
 app.use(express.static('data'))
+app.use('/test', express.static('test'))
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 app.get('/', function (req, res) {
+  const args = process.argv.slice(2)
+  if (args[0] === 'test') {
+    res.sendFile(__dirname + '/test/index.html')
+    return
+  }
   res.sendFile(__dirname + '/index.html')
 })
+
