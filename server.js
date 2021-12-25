1362693ff3b7bc6a2e22716328f84eb34d72a3bd
choi.sungryeol <choi.sungryeol@navercorp.com>
2021-12-25 22:27:41 +0900

    test: change data route & add right hand fixer example

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('data'))
+app.use('/data', express.static('data'))
 app.use('/test', express.static('test'))
 
 app.listen(port, () => {
