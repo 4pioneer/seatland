f691c1108d4f2d3335eb2354b2e6e2d6748763b7
choi.sungryeol <choi.sungryeol@navercorp.com>
2022-02-02 17:25:29 +0900

    feat: #7 add astar example

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.use('/data', express.static('data'))
-app.use('/test', express.static('test'))
+app.use('/data', express.static('data'));
+app.use('/test', express.static('test'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
 app.get('/', function (req, res) {
-  const args = process.argv.slice(2)
+  const args = process.argv.slice(2);
   if (args[0] === 'test') {
-    res.sendFile(__dirname + '/test/index.html')
+    res.sendFile(__dirname + '/test/index.html');
     return
   }
   res.sendFile(__dirname + '/index.html')
-})
+});
 
